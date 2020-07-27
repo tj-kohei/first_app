d797ef5a85ae9a71086dafe33842c07ed4b44171
tj-kohei <68324989+tj-kohei@users.noreply.github.com>
2020-07-27 15:12:43 +0900

    既読機能のフロント側の実装

---
+++ app/javascript/checked.js
@@ -0,0 +1,36 @@
+function check () {
+  const posts = document.getElementsByClassName("post");
+  postsA = Array.from(posts);
+
+  postsA.forEach(function (post) {
+    if (post.getAttribute("data-load") != null){
+      return null;
+    }
+    post.set("data-load","ture");
+    post.addEventListener("click", (e) => { 
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET",`/posts${postId}`,true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const item = XHR.response.post;
+        if (item.checked === true){
+          post.setAttribute("data-check");
+        }
+        if (XHR.status != 200){
+          alert(`Error${XHR.status}: ${XHR.statusText}`);
+        } else {
+          return null;
+        }
+      }
+      XHR.onerror = () => {
+        alert("Request failed");
+      };
+      
+      e.preventDefault();
+    });
+  });
+}
+
+setInterval(sheck, 1000);
